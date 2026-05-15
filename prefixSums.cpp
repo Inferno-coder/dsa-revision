2cbfaab82723d43935ef255ec330348c090dfb83
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2026-05-15 22:31:04 +0530

    Update prefixSums.cpp

--- prefixSums.cpp
+++ prefixSums.cpp
@@ -159,3 +159,28 @@ public:
         return res;
     }
 };
+
+//Range sum query 2D
+class NumMatrix {
+public:
+    vector<vector<int>>pre;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size();
+        pre.resize(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int left=j>0 ?pre[i][j-1]:0;
+                int top=i>0?pre[i-1][j]:0;
+                int common=(i>0 && j>0)?pre[i-1][j-1]:0;
+                pre[i][j]=matrix[i][j]+left+top-common;
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int left=col1>0?pre[row2][col1-1]:0;
+        int top= row1>0?pre[row1-1][col2]:0;
+        int common=(row1>0 && col1>0)?pre[row1-1][col1-1]:0;
+        return pre[row2][col2]-left-top+common;
+    }
+};
