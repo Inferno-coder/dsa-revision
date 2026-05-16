6aacadd2d01a5160a968b0464d53a7b3e545eef1
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2026-05-16 14:43:45 +0530

    Update prefixSums.cpp

--- prefixSums.cpp
+++ prefixSums.cpp
@@ -277,3 +277,18 @@ public:
         return true;
     }
 };
+
+
+//Corporate flight bookings
+ vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int>flights(n,0);
+        for(auto it:bookings){
+            flights[it[0]-1]+=it[2];
+            if(it[1]<n)flights[it[1]]-=it[2];
+        }
+        for(int i=1;i<n;i++){
+            flights[i]=flights[i]+flights[i-1];
+        }
+        return flights;
+}
+
