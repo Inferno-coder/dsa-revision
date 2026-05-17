e2758fdfb8b17dd2da13c8f5c2a5270a995e3daa
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2026-05-17 17:37:35 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,1 +1,16 @@
-//recursion problems
+//Pow()
+
+class Solution {
+public:
+    double solvePow(double x,int n){
+        if(n==0)return 1.0;
+        double half=solvePow(x,n/2);
+        if(n%2==0)return half*half;
+        else return half*half*x;
+    }
+    double myPow(double x, int n) {
+        if(n<0)return 1/solvePow(x,n);
+        return solvePow(x,n);
+    }
+};
+
